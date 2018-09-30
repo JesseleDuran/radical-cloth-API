0968e9c363a92a1e3f0ab94032fffbc89c7dcfb4
Jessele Duran <jesseleadt@gmail.com>
2018-09-30 11:05:55 -0400

    this commit is a lie

---
+++ database/migrations/1538259761694_favoritos_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class FavoritosSchema extends Schema {
+  up () {
+    this.create('favoritos', (table) => {
+      table.increments()
+      table.integer('user_id').notNullable().unsigned().references('id').inTable('users')
+      table.integer('producto_id').notNullable().unsigned().references('id').inTable('producto')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('favoritos')
+  }
+}
+
+module.exports = FavoritosSchema
---
+++ database/migrations/1538260073570_producto_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ProductoSchema extends Schema {
+  up () {
+    this.create('productos', (table) => {
+      table.increments()
+      table.float('precio').unsigned()
+      table.string('nombre', 254).notNullable().unique()
+      table.integer('dias_hacer').unsigned()
+      table.text('descripcion')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('productos')
+  }
+}
+
+module.exports = ProductoSchema
