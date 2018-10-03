ef01ab4c316c78c22ffb3b3c23338cf4c2de6bcc
Jessele Duran <jesseleadt@gmail.com>
2018-10-02 22:51:22 -0400

    add token in register endpoint

--- README.md
+++ README.md
@@ -26,3 +26,10 @@ Run the following command to run startup migrations.
 ```js
 adonis migration:run
 ```
+
+### To run the server just run:
+
+```
+adonis serve --dev
+```
+
--- app/Controllers/Http/Auth/UserController.js
+++ app/Controllers/Http/Auth/UserController.js
@@ -4,7 +4,7 @@ const User = use('App/Models/User')
 const Hash = use('Hash')
 class UserController {
 
-    async register ({request, response}) {
+    async register ({request, response, auth}) {
         const userInfo = request.only(['username', 'password', 'email'])
 
         const user = new User()
@@ -13,8 +13,13 @@ class UserController {
         user.password = userInfo.password
         user.email = userInfo.email
 
-        await user.save()
-        return response.status(201).json(user)
+        try {
+            await user.save()
+            let token = await auth.attempt(userInfo.username, userInfo.password)
+            return response.status(201).json({"auth":token, "user": user})
+        } catch (error) {
+            return response.status(401).send({"error": error.message})
+        }
     }
 
     async login ({request, auth, response}) {
