a55399424a7edf7e5a8d03b1a098fae53a44c17a
Jessele Duran <jesseleadt@gmail.com>
2018-10-23 18:18:22 -0400

    update pedidos to existing products

--- database/migrations/1540264509870_pedido_schema.js
+++ database/migrations/1540264509870_pedido_schema.js
@@ -7,11 +7,12 @@ class PedidoSchema extends Schema {
     this.create('pedidos', (table) => {
       table.increments()
       table.integer('cliente_id').notNullable().unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('producto_id').unsigned().references('id').inTable('producto').onDelete('CASCADE')
       table.integer('chat_id').notNullable().unsigned().references('id').inTable('chat').onDelete('CASCADE')
       table.string('talla', 254).notNullable()
       table.string('color', 254).notNullable()
       table.string('foto', 254).notNullable()
-      table.boolean('is_terminado').notNullable()
+      table.boolean('is_terminado').notNullable().defaultTo(false)
       table.timestamps()
     })
   }
--- start/routes.js
+++ start/routes.js
@@ -41,6 +41,7 @@ Route.get('/logout', 'Auth/UserController.logout').middleware(['auth'])
 
 Route.group(() => {
   Route.post('/create', 'PedidoController.create').middleware(['auth'])
+  Route.get('/getAll', 'PedidoController.index').middleware(['auth'])
 }).prefix('/pedido')
 
 
